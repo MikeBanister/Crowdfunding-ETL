5880088a6766cdd427ea8dadfe54a4a2af3038da
MikeBanister <118936528+MikeBanister@users.noreply.github.com>
2023-05-18 23:26:28 -0600

    2023-05-18-23:26

--- Crowdfunding_ETL.sql
+++ Crowdfunding_ETL.sql
@@ -9,43 +9,60 @@ CREATE TABLE "campaign" (
     "company_name" VARCHAR   NOT NULL,
     "description" VARCHAR   NOT NULL,
     "goal" VARCHAR   NOT NULL,
-    "pledged" INT   NOT NULL,
+    "pledged" FLOAT8   NOT NULL,
     "outcome" VARCHAR   NOT NULL,
-    "backers_count" INT   NOT NULL,
+    "backers_count" FLOAT8   NOT NULL,
     "country" VARCHAR   NOT NULL,
     "currency" VARCHAR   NOT NULL,
-    "launch_date" date   NOT NULL,
-    "end_date" date   NOT NULL,
-    "category_id" INT   NOT NULL,
-    "subcategory_id" INT   NOT NULL,
+    "launch_date" DATE   NOT NULL,
+    "end_date" DATE   NOT NULL,
+    "category_id" VARCHAR   NOT NULL,
+    "subcategory_id" VARCHAR   NOT NULL,
     CONSTRAINT "pk_campaign" PRIMARY KEY (
         "cf_id"
      )
 );
 
 CREATE TABLE "category" (
-    "category_id" INT   NOT NULL,
-    "category" VARCHAR   NOT NULL
+    "category_id" VARCHAR   NOT NULL,
+    "category" VARCHAR   NOT NULL,
+    CONSTRAINT "pk_category" PRIMARY KEY (
+        "category_id"
+     )
 );
 
 CREATE TABLE "subcategory" (
-    "subcategory_id" INT   NOT NULL,
-    "subcategory" VARCHAR   NOT NULL
+    "subcategory_id" VARCHAR   NOT NULL,
+    "subcategory" VARCHAR   NOT NULL,
+    CONSTRAINT "pk_subcategory" PRIMARY KEY (
+        "subcategory_id"
+     )
 );
 
 CREATE TABLE "contacts" (
     "contact_id" INT   NOT NULL,
     "first_name" VARCHAR   NOT NULL,
     "last_name" VARCHAR   NOT NULL,
-    "email" VARCHAR   NOT NULL
+    "email" VARCHAR   NOT NULL,
+    CONSTRAINT "pk_contacts" PRIMARY KEY (
+        "contact_id"
+     )
 );
+
+ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_contact_id" FOREIGN KEY("contact_id")
+REFERENCES "contacts" ("contact_id");
 
 ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_category_id" FOREIGN KEY("category_id")
 REFERENCES "category" ("category_id");
 
-ALTER TABLE "subcategory" ADD CONSTRAINT "fk_subcategory_subcategory_id" FOREIGN KEY("subcategory_id")
-REFERENCES "campaign" ("subcategory_id");
+ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_subcategory_id" FOREIGN KEY("subcategory_id")
+REFERENCES "subcategory" ("subcategory_id");
 
-ALTER TABLE "contacts" ADD CONSTRAINT "fk_contacts_contact_id" FOREIGN KEY("contact_id")
-REFERENCES "campaign" ("contact_id");
+SELECT * FROM campaign
+SELECT * FROM category
+SELECT * FROM contacts
+SELECT * FROM subcategory
+
+
+
 
